ebef999eb099cdad5835e7630b0199c9f5ae893c
Temirlan <bamblebee988@gmail.com>
2019-05-10 17:11:37 +0600

    add nouislider

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+var range = document.getElementById('range');
+
+  noUiSlider.create(range, {
+      start: [4000],
+      step: 1000,
+      range: {
+          'min': [2000],
+          'max': [10000]
+      }
+  });
+
 $(document).ready(function() {
-  // $('.js-skill__range').val(4).change();
+  
 });
